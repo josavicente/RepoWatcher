9417147631f09d8cd65717d09fab9ded49335cfd
Josa Vicente <jvicenpe@gmail.com>
2023-01-27 20:42:39 +0100

    Initial Commit

---
+++ RepoWatcher/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RepoWatcher
+//
+//  Created by Josafat Vicente Pérez on 27/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Josafat Vicente Pérez on 27/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
